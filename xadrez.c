ee83cb6677de75b90204805805618b41acd3a77a
gabriel987165-lab <gabriel987165@gmail.com>
2025-09-01 22:38:53 +0000

    Nivel Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,42 +1,60 @@
 #include <stdio.h>
 
-int main() {
-    // ------------------- TORRE -------------------
-    printf("Movimento da Torre:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); // cada passo para a direita
+// TORRE
+void torre(int i) {
+    if (i <= 0) return;
+    printf("Direita\n");
+    torre(i - 1);
+}
+
+// BISPO
+void bispo(int j) {
+    if (j >= 5) return;
+
+    for (int i = 0; i < 1; i++) {
+        for (int k = 0; k < 1; k++) {
+            printf("Cima Direita\n");
+        }
     }
 
-    // ------------------- BISPO -------------------
-    printf("\nMovimento do Bispo:\n");
-    int j = 0;
-    while (j < 5) {
-        printf("Cima Direita\n"); // diagonal = duas direções
-        j++;
-    }
+    bispo(j + 1);
+}
 
-    // ------------------- RAINHA -------------------
-    printf("\nMovimento da Rainha:\n");
-    int k = 0;
-    do {
-        printf("Esquerda\n");
-        k++;
-    } while (k < 8);
-  
-   
-    // ------------------- CAVALO -------------------
-    printf("\nMovimento da Cavalo:\n");
-     int L = 1; 
+// RAINHA
+void rainha(int k) {
+    if (k <= 0) return;
+    printf("Esquerda\n");
+    rainha(k - 1);
+}
 
+// CAVALO
+void cavalo() {
+    int L = 1;
     while (L--) {
         for (int i = 0; i < 2; i++) {
-            printf("Baixo\n");  
+            printf("Cima\n");
         }
-        printf("Esquerda\n");   
-        printf("\n ");
+        printf("Direita\n");
+        printf("\n");
     }
+}
 
-    
+int main() {
+    // TORRE
+    printf("Movimento da Torre:\n");
+    torre(5);
+
+    // BISPO
+    printf("\nMovimento do Bispo:\n");
+    bispo(0);
+
+    // RAINHA
+    printf("\nMovimento da Rainha:\n");
+    rainha(8);
+
+    // CAVALO
+    printf("\nMovimento do Cavalo:\n");
+    cavalo();
 
     return 0;
 }
